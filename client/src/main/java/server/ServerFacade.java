9c0a1b79bbb210c519050e753032ab3953e9e210
Ben Christensen <benjaminb.christensen@outlook.com>
2024-03-19 18:53:19 -0600

    Register complete

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,74 @@
+package server;
+
+import com.google.gson.Gson;
+import model.AuthDataModel;
+import model.UserDataModel;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+    private String authToken;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthDataModel register(String username, String password, String email) throws Exception {
+        var path = "/user";
+        var user = new UserDataModel(username, password, email);
+        var auth = this.makeRequest("POST", path, user, AuthDataModel.class);
+        if (auth != null) {
+            authToken = auth.authToken();
+        } else {
+            throw new Exception("invalid");
+        }
+        return auth;
+    }
+
+    public AuthDataModel login()
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            var status = http.getResponseCode();
+            if (!(status/100 == 2)){
+                throw new Exception("failure: " + status);
+            }
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package clientTests;
 
+import dataAccess.SQLUserDAO;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+import service.ClearService;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -22,8 +29,27 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerTest(){
+        try{
+            ClearService.clearDatabase();
+            var auth = facade.register("user", "password", "email@email.com");
+            System.out.println(auth);
+            Assertions.assertNotEquals(auth.authToken(), "");
+            Assertions.assertEquals(SQLUserDAO.length(), 1);
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+    @Test
+    public void badRegisterTest(){
+        try{
+            ClearService.clearDatabase();
+            var auth = facade.register("", "k", "email");
+        }
+        catch (Exception e){
+            Assertions.assertEquals(2, 2);
+        }
     }
 
 }
