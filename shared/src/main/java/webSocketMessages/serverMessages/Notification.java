74a8c70fa9fb6cecabbe2abea3188a4b66f5e645
Ben Christensen <benjaminb.christensen@outlook.com>
2024-04-08 16:50:07 -0600

    Notification

---
+++ shared/src/main/java/webSocketMessages/serverMessages/Notification.java
@@ -0,0 +1,10 @@
+package webSocketMessages.serverMessages;
+
+public class Notification extends ServerMessage{
+    String message;
+
+    public Notification(String message){
+        super(ServerMessageType.NOTIFICATION);
+        this.message = message;
+    }
+}
